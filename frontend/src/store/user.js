54294f3d97aac3ceb8e4ac0eecdd15471e9f3565
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-04-13 17:38:43 +0300

    Создание getter isUserLoggedIn

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -79,6 +79,9 @@ export default {
     getters: {
         user(state) {
             return state.user
+        },
+        isUserLoggedIn (state) {
+            return state.user !== null
         }
     }
 
